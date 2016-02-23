f81dbb76c17e5f4bed63e2edb753f230002c3cfe
jshields310 <jshields310@gmail.com>
2016-02-23 12:12:41 -0500

    Update masterTMS.ino
    Added outlines for major classes and functions. #included thread.h

--- masterTMS.ino
+++ masterTMS.ino
@@ -6,17 +6,47 @@
     15 February 2016:::::Created repository and started basic code setup for the program.
 */
 
-float wt;
+#include <stdio.h>
+#include <thread.h>     // using threads
+#define DOUT 11
+#define CLK 12
+
+using namespace std;
+
+class Entity
+{
+    float wt;
+    int tm;
+  // class for anything detected by sensor  
+};
+
+class Vehicle :: Entity
+{
+    // class for only Vehicles that have been verified. Inherits Entity
+};
+
+void receive()
+{
+    // receive Entities, check weight, determine Vehicles, add to queue.
+}
+
+void process()
+{
+    // process queue by time of arrival, with error checks in place
+}
+
+
+
+float c_factor = -7050.0;   // used Calibration test to determine this.
 
 void setup()  // setup() runs once at the start
 {
+    pinMode(DOUT, INPUT);
+    pinMode(CLK, INPUT);
     pinMode(13, OUTPUT);
-    pinMode(A0, INPUT); // unsure if this works, will test once the prototype is complete.
 }
 
 void loop()   // will loop for the remainder of the program's runtime
 {
-    digitalWrite(13, LOW);  // set LED to off
-    
-    wt = analogRead(A0);  // unsure; trying to read the weight value detected by the load cell in pin A0.
+    thread
 }
