3abb38a13471e2087bf32f575e8a76bc7b852037
jshields310 <jshields310@gmail.com>
2016-02-23 13:26:23 -0500

    Update masterTMS.ino
    Added more functions, edited the timer function and added a (rough) timer reset line to process(). Also added initial Preconditions and Postconditions for every function in right now.

--- masterTMS.ino
+++ masterTMS.ino
@@ -1,15 +1,51 @@
 /*
     Traffic Management System
     Master Program
+    Joshua Shields, Levi Whited, Roberto Roux
     v 0.0.1 
     
     15 February 2016:::::Created repository and started basic code setup for the program.
+*/
+
+/* 
+    int timer()
+        Precondition: N/A. 
+        Postcondition: t is incremented and returned. Can be called to simply increment the time or grab the current time
+        
+    void receive()
+        Precondition: Strain Gauges hooked up and working correctly.
+        Postcondition: Any entity is detected by the Strain Gauges and their weight is tested. They are either put into the
+                        Vehicle[] queue or thrown out (by way of error signalling)
+                        
+    void process()
+        Precondition: receive() has detected at least a single Vehicle and placed it in the Vehicle[] queue
+        Postcondition: Vehicle[] queue is processed, resulting in either an empty queue or one less element in the array.
+        
+    void error_signal()
+        Precondition: An error is detected somewhere specific in the program
+        Postcondition: LED is blinked rapidly to signify an error (or RED if more LEDs are purchased)
+        
+    void go_signal()
+        Precondition: Vehicle has been taken from the queue and given the go ahead
+        Postcondition: LED is shone without blinking to signify (or GREEN if more LEDs are purchased)
+        
+    void setup()
+        Precondition: N/A. Runs once at start of program
+        Postcondition: Corresponding pins are now set for their specific duties, Serial is ready to be used.
+        
+    void loop()
+        Precondition: N/A. Runs continously throughout duration of the program.
+        Postcondition: Program is running
 */
 
 #include <stdio.h>
 #include <thread.h>     // using threads
 #define DOUT 11
 #define CLK 12
+#define QUEUESIZE 5
+
+int t = 0;  // global variable to be used as time.
+float c_factor = -7050.0;   // used Calibration test to determine this.
 
 using namespace std;
 
@@ -23,7 +59,44 @@ class Entity
 class Vehicle :: Entity
 {
     // class for only Vehicles that have been verified. Inherits Entity
+    // may not be needed actually
 };
+
+
+void setup()  // setup() runs once at the start
+{
+    pinMode(DOUT, INPUT);
+    pinMode(CLK, INPUT);
+    pinMode(13, OUTPUT);
+    
+    Serial.begin(9600); // Serial is used to output results to the Serial Monitor
+}
+
+void error_signal()
+{
+    // blinks LED rapidly if an error has occured.
+}
+
+void go_signal()
+{
+    // shines LED with no blinking if process has completed.
+}
+
+bool check_queue(Vehicle[] *q)
+{
+    // simple function to go through the Vehicle queue and see if it is empty or not
+    bool empty = false;
+    
+    for(int i = 0; i <= QSIZE; i++)
+    {
+        if(q[i] == 0)   // look into better way to write this, like without a for loop. For now this is fine.
+            empty = true;
+        else
+            empty = false;
+    }
+    
+    return empty;   
+}
 
 void receive()
 {
@@ -33,20 +106,33 @@ void receive()
 void process()
 {
     // process queue by time of arrival, with error checks in place
+    int time_since_last = 0;
+    if(time_since_last > 120 && Vehicle)
 }
 
-
-
-float c_factor = -7050.0;   // used Calibration test to determine this.
-
-void setup()  // setup() runs once at the start
+int timer(bool reset)
 {
-    pinMode(DOUT, INPUT);
-    pinMode(CLK, INPUT);
-    pinMode(13, OUTPUT);
+    // funtion that keeps track of time from the beginning of the run. Resets after 120 seconds of no activity and an empty
+    // queue.
+    
+    Serial.println("Time is: ");
+    Serial.print(t);
+    
+    bool r = reset;
+    
+    if(reset)
+        t = 0;
+        
+    delay(1000);    // increment timer every second. OPEN TO CHANGE AFTER TESTING
+    t++;
+    
+    return t;
 }
 
 void loop()   // will loop for the remainder of the program's runtime
 {
-    thread
+    thread receive();   
+    thread process();
+    thread timer();
+    // Concurrently receives new Vehicles into the queue while the queue is processed, and increments the timer
 }
