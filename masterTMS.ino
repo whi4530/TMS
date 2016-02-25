e86322bb404fb6ec18cd9b0178bac8b132e365bc
jshields310 <jshields310@gmail.com>
2016-02-25 12:02:40 -0500

    Update masterTMS.ino
    - Added con and decon for Entity
    - worked on receive()
    - sensor detects weight above specific value (TOOLIGHT) and creates an Entity with that weight and the current time according to timer()

--- masterTMS.ino
+++ masterTMS.ino
@@ -5,6 +5,9 @@
     v 0.0.1 
     
     15 February 2016:::::Created repository and started basic code setup for the program.
+    
+    25 Feb 2016:::::Added con and decon for Entity, will add Vehicle's soon. Started real work on receive() to detect any weight at
+                    all, next is to validate them and turn them into Vehicles.
 */
 
 /* 
@@ -47,22 +50,41 @@
 #define C1 12
 #define D2 13
 #define C2 14
+
 #define D3 15
 #define C3 16
+#define D4 17
+#define C4 18
+
+#define D5 19
+#define C5 20
+#define D6 21
+#define C6 22
+
+#define D7 23
+#define C7 24
+#define D8 25
+#define C8 26
 
 #define QUEUESIZE 5
 #define VALDELAY 3000   // delay in ms. Will cause the process complete signal to run for # of ms given (default 3000)
 #define ERRDELAY 30     // must be VALDELAY / 100 for consistency reasons.
+#define VALIDWT 100     // 100 is placeholder until we weigh the RC cars we will use. This is in lbs.
 
-HX711 fsensors[4];  // array for sensors closest to stop sign
-HX711 bsensors[4];  // array for sensors farther back
+HX711 fsensors[4] = {(D1, C1), (D3, C3), (D5, C5), (D7, C7)};  // array for sensors closest to stop sign
+HX711 bsensors[4] = {(D2, C2), (D4, C4), (D6, C6), (D8, C8)};  // array for sensors farther back
 
 using namespace std;
 
 class Entity
 {
-    float wt;
-    int tm;
+    public:
+        float wt;
+        int tm;
+        
+    private:
+        Entity();       // constructor and deconstructor for nicer code
+        ~Entity();
   // class for anything detected by sensor  
 };
 
@@ -73,9 +95,10 @@ class Vehicle :: Entity
     int prec;   // optional precedence variable
 };
 
-int t = 0;  // global variable to be used as time.
+unsigned long t = 0;  // global variable to be used as time.
 float c_factor = -7050.0;   // used Calibration test to determine this.
 
+Entity ents[10] = {0};  // probably needs to be dynamically allocated.
 Vehicle queue[QUEUESIZE] = {0};
 
 
@@ -89,6 +112,12 @@ void setup()  // setup() runs once at the start
     pinMode(13, OUTPUT);
     
     Serial.begin(9600); // Serial is used to output results to the Serial Monitor
+    
+    fsensors.set_scale(c_factor);
+    bsensors.set_scale(c_factor);   // set scales' calibration factors to use the data it gives us
+    
+    fsensors.tare();
+    bsensors.tare();    // set scales to 0
 }
 
 void error_signal()
@@ -112,25 +141,31 @@ void go_signal()
     delay(SIGDELAY);
 }
 
-bool check_queue(Vehicle[] *q)
-{
-    // simple function to go through the Vehicle queue and see if it is empty or not
-    bool empty = false;
-    
-    for(int i = 0; i <= QSIZE; i++)
-    {
-        if(q[i] == 0)   // look into better way to write this, like without a for loop. For now this is fine.
-            empty = true;
-        else
-            empty = false;
-    }
-    
-    return empty;   
-}
-
 void receive()
 {
     // receive Entities, check weight, determine Vehicles, add to queue.
+    int ctr = 0, detwt[4] = {0};
+    
+    if(detwt[0] = (fsensors[0].get_units) > TOOLIGHT)
+    {
+        ents[ctr] = new Entity(detwt[0], timer());
+        ctr++;
+    }
+    if(detwt[1] = (fsensors[1].get_units) > TOOLIGHT)
+    {
+        ents[ctr] = new Entity(detwt[1], timer());
+        ctr++;
+    }
+    if(detwt[2] = (fsensors[2].get_units) > TOOLIGHT)
+    {
+        ents[ctr] = new Entity(detwt[2], timer());
+        ctr++;
+    }
+    if(detwt[3] = (fsensors[3].get_units) > TOOLIGHT)
+    {
+        ents[ctr] = new Entity(detwt[3], timer());
+        ctr++;
+    }
 }
 
 void process()
@@ -142,13 +177,13 @@ void process()
         
 }
 
-int timer(bool reset)
+unsigned long timer(bool reset)
 {
     // funtion that keeps track of time from the beginning of the run. Resets after 120 seconds of no activity and an empty
     // queue.
     
-    Serial.println("Time is: ");
-    Serial.print(t);
+    //Serial.println("Time is: ");
+    //Serial.print(t);
     
     bool r = reset;
     
@@ -159,6 +194,18 @@ int timer(bool reset)
     t++;
     
     return t;
+}
+
+Entity::Entity(float f, unsigned long l)
+{
+    wt = f;
+    tm = l;
+}
+
+Entity::~Entity()
+{
+    delete[] wt;
+    delete[] tm;
 }
 
 void loop()   // will loop for the remainder of the program's runtime
